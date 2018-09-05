ba58a6b6682fee2275d479e8322968ed8e54617a
Jitendra Selvam <jitendra3195@gmail.com>
2018-09-04 23:52:11 -0400

    added thrift file

---
+++ FileUploadAPI/thrift/UploadService.thrift
@@ -0,0 +1,26 @@
+namespace java com.fileUplaod.Jitendra
+
+enum TransferType {
+    REQUEST = 0,
+    PROGRESS
+}
+
+struct TransferInfo {
+    1: TransferType type,
+    2: string fileName
+    3: i64 length,
+    4: binary data
+}
+
+service EchoService {
+    string echo(1: string input)
+}
+
+service DownloadService {
+    TransferInfo download(1:TransferInfo info),
+    list<string> getFileList()
+}
+
+service UploadService {
+    void upload(1: TransferInfo info),
+}
